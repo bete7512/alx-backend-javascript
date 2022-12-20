975d2f302d6304c147f87bbbe8ec602121059173
bete7512 <betekbebe@gmail.com>
2022-12-20 09:03:08 +0300

    one things

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,12 @@ function getCurrentYear () {
 }
 
 export default function getBudgetForCurrentYear (income, gdp, capita) {
-  const budget = {};
+  const budget = {
+    [`income-${getCurrentYear()}`] : income,
+    [`gdp-${getCurrentYear()}`] : gdp,
+    [`capita-${getCurrentYear()}`] : capita
+  };
 
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
 
   return { ...budget };
 }

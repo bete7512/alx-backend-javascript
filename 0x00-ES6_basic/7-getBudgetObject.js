857bfbe0ab0ba53c3c83c86712a8987cf1ba01b8
bete7512 <betekbebe@gmail.com>
2022-12-20 08:58:54 +0300

    many es6 tricks

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,8 @@
 export default function getBudgetObject (income, gdp, capita) {
   const budget = {
-    income: income,
-    gdp: gdp,
-    capita: capita
+    income,
+    gdp,
+    capita
   };
 
   return { ...budget };

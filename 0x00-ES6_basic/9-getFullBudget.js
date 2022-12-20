f0fed43715954aa5f4a25d9e12cef160604bef7c
bete7512 <betekbebe@gmail.com>
2022-12-20 16:37:00 +0300

    messages for commit

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,12 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) { return `${income}`; },
-    getIncomeInEuros(income) { return `${income} euros`; },
+    getIncomeInDollars(income) {
+      return `${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
   };
 
   return fullBudget;

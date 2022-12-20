5c3fe84b96f7a14a9b602be4244d176ee9438da7
bete7512 <betekbebe@gmail.com>
2022-12-20 09:23:20 +0300

    messages for commit

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments (employeesList) {
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
-    }
+    },
   };
 }

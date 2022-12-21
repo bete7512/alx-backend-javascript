c224f3afe6cc43fcb422912ba68c8657bc55ccf9
bete7512 <betekbebe@gmail.com>
2022-12-21 20:36:25 +0300

    message for promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,17 @@
 function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
+  //       })
+  //  new Promise((resolve, reject) => {
+  //     if (promise) {
+  //       resolve({
+  //         status: 200,
+  //         body: 'Success',
+  //       });
+  //       console.log('Got a response from the API');
+  //     } else {
+  //       reject(new Error());
+  //     }
+  //   });
 }
 
 export default handleResponseFromAPI;

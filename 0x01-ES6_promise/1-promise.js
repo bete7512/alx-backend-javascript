9c75b7804de7c208b2a4f264be37da6ba8b6bf91
bete7512 <betekbebe@gmail.com>
2022-12-21 20:17:07 +0300

    one Two

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,13 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject('The fake API is not working currently');
-    }
+    if (success === true) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
   });
 }
 

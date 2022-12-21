5d226dfaa0e4690010c87f1f6608da851478cdef
bete7512 <betekbebe@gmail.com>
2022-12-21 21:32:20 +0300

    multiple promise return value

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
+    console.log(`${values[0].firstName} ${values[0].lastName} ${values[1].body}`);
+  }).catch(() => {});
+}

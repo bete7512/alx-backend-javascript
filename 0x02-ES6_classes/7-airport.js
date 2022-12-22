6e8ddcd212799419658fa677866414abd6b438cf
bete7512 <betekbebe@gmail.com>
2022-12-22 23:04:28 +0300

    special

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -5,8 +5,8 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
-
-  get [Symbol.toStringTag]() {
-    return this._code;
-  }
+// I don't know how to implement this part
+//   get [Symbol.toStringTag]() {
+//     return this._code;
+//   }
 }

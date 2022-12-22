cda4fbc84f57158b8347b45a567eb8563aa81cd3
bete7512 <betekbebe@gmail.com>
2022-12-22 22:38:53 +0300

    building check for abstract classes of javascript classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,18 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number' && this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') throw Error('Class extending Building must override evacuationWarningMessage');
-    this._sqft = sqft;
   }
 }

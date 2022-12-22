9cdd4f109903cb817b124b6c851a8110c9929581
bete7512 <betekbebe@gmail.com>
2022-12-22 14:35:10 +0300

    specia;

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
+    if(typeof maxStudentsSize !== 'number') throw TypeError('maxStudentsSize must be a number')
     this._maxStudentsSize = maxStudentsSize;
   }
 }

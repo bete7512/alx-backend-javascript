1eaa2a29d92b2c5dc51d506620db9e1691d31317
bete7512 <betekbebe@gmail.com>
2023-01-05 22:21:07 +0300

    manuscript from Yostina

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    throw Error('Position outside range');
-  }
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
+  const view = new DataView(buffer, 0);
+  if (position > length - 1) throw Error('Position outside range');
+  view.setInt8(position, value);
   return view;
 }

8b842d031e4f2459003691c344de074277babe35
bete7512 <betekbebe@gmail.com>
2023-01-05 22:18:59 +0300

    some times make crazy

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  view[position] = value;
+  return view;
+}

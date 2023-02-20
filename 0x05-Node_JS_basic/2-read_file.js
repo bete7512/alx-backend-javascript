ef482ac9354c6a7df7d3a005638f5446dbf06662
bete7512 <betekbebe@gmail.com>
2023-02-20 22:10:55 +0300

    readfile from file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const students = data.split('\n');
+    console.log(`Number of students: ${students.length - 1}`);
+    students.shift();
+    let cs = 0;
+    let swe = 0;
+    for (let i = 1; i < students.length; i++) {
+      if (students[i].includes('CS')) {
+        cs++;
+      } else {
+        swe++;
+      }
+    }
+    console.log(
+      `Number of students in CS: ${cs}. List: ${students
+        .filter((student) => student.includes('CS'))
+        .map((student) => student.split(',')[0])
+        .join(', ')}`
+    );
+    console.log(
+      `Number of students in SWE: ${swe}. List: ${students
+        .filter((student) => student.includes('SWE'))
+        .map((student) => student.split(',')[0])
+        .join(', ')}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+module.exports = countStudents;

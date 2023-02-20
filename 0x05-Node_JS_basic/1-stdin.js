dc156a322006f5d4eb8a5a64c0c58180041c8bf0
bete7512 <betekbebe@gmail.com>
2023-02-20 21:34:45 +0300

    messages semistandard fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.setEncoding("utf8");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const fname = process.stdin.read();
   if (fname) process.stdout.write(`Your name is: ${fname}`);
 });
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });

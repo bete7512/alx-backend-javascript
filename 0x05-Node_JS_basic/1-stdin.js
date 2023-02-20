5c1acd5ee41b5b70686bbad51a253d23e6942948
bete7512 <betekbebe@gmail.com>
2023-02-20 21:33:28 +0300

    messages for something I have not

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,10 @@
-const readline = require("readline");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.setEncoding("utf8");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on("readable", () => {
+  const fname = process.stdin.read();
+  if (fname) process.stdout.write(`Your name is: ${fname}`);
 });
-const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-
-// Usage inside aync function do not need closure demo only*
-(async () => {
-  try {
-    // console.log("");
-    const name = await prompt("Welcome to Holberton School, what is your name?\n");
-
-    // Can use name for next question if needed
-    // const lastName = await prompt(`Hello ${name}, what's your last name?: `);
-    // Can prompt multiple times
-    console.log("Your name is:", name);
-    console.log("This important software is now closing");
-    rl.close();
-  } catch (e) {
-    console.error("Unable to prompt", e);
-  }
-})();
-
-// When done reading prompt, exit program
-rl.on("close", () => process.exit(0));
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
